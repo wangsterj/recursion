ab4bf7e127ef21d32fe147a58e573e868fd8370e
amn <wang.j@berkeley.edu>
2019-06-30 17:35:34 -0700

    Recursion - StringifyJSON
    Completed stringifyJSON function and passed all specs.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+ 	if (typeof(obj) === "number" || typeof(obj) === "boolean") {
+		return obj.toString();
+	} else if (obj === null) {
+		return "" + obj;
+	} else if (typeof(obj) === "string") {
+		return "\""+ obj + "\"";
+ 	} else if (Array.isArray(obj)) {
+ 		var stringified = "[";
+ 		for (var i =0; i<obj.length; i++) {
+ 			if (i !=obj.length -1)
+ 				stringified += stringifyJSON(obj[i]) + ',';
+ 			else
+ 				stringified += stringifyJSON(obj[i]);
+ 		}
+		return stringified + "]";
+	} else {
+		var objkeys = Object.keys(obj);
+		if (objkeys.length == 0) {
+			return "{}";
+		}
+		var stringified = "{";
+		for (var i = 0; i < objkeys.length; i++) {
+			if ( obj[objkeys[i]] !== undefined && typeof obj[objkeys[i]] != "function")
+				if ( i != objkeys.length - 1)
+					stringified += "\"" + objkeys[i] + "\":" + stringifyJSON(obj[objkeys[i]]) + ',';
+				else
+					stringified += "\"" + objkeys[i] + "\":" + stringifyJSON(obj[objkeys[i]]);
+		}
+		return stringified +"}";
+	}
 };
